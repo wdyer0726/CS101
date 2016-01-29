69ff669baa9dfaecad9aa3e210d62f2c4c62c1ee
wdyer0726 <wdyer0726@gmail.com>
2016-01-29 12:16:21 -0800

    Update webcrawler.py

--- udacity/webcrawler/webcrawler.py
+++ udacity/webcrawler/webcrawler.py
@@ -74,18 +74,39 @@ def get_all_links(page):
         else:
             break
     return links
-#to find max_depth we need to introduce new list that tracks the pages
+    
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
+    
+def lookup(index,keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]        
+    return []
+
+def add_page_to_index(index,url,content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
 def crawl_web(seed,max_depth):
     tocrawl = [seed]
     crawled = []
     next_depth = []
     depth = 0
+    index = []
     while tocrawl and depth <= max_depth:
         page = tocrawl.pop()
         if page not in crawled:
-            union(next_depth, get_all_links(get_page(page)))
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(next_depth, get_all_links(content))
             crawled.append(page)
         if not tocrawl:
             tocrawl, next_depth = next_depth, []
             depth=depth + 1
-    return crawled
+    return index
